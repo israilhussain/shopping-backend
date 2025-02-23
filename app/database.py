a5dd4d81ec581ab9304061a6a08fec793757f0ea
Md Israil <md.israil@nagarro.com>
2025-02-23 16:05:40 +0530

    add product create API

--- app/database.py
+++ app/database.py
@@ -3,11 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
+
 # Load environment variables
 load_dotenv()
 
-# Database connection string
-DATABASE_URL = f"postgresql+asyncpg://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+
+ENVIRONMENT = os.getenv("ENVIRONMENT", "local")
+
+if ENVIRONMENT == "aws":
+    DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+else:
+    DATABASE_URL = f"postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:5432/shopping-db"
+
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
--- app/utils.py
+++ app/utils.py
@@ -4,15 +4,21 @@ import os
 
 AWS_REGION = os.getenv("AWS_REGION")
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME")
+AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+s3_client = boto3.client("s3",
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
+)
 
-s3_client = boto3.client("s3", region_name=AWS_REGION)
 
 def upload_image_to_s3(file):
     """Uploads an image file to S3 and returns the S3 URL."""
     file_extension = file.filename.split(".")[-1]
     file_key = f"products/{uuid.uuid4()}.{file_extension}"
-
-    s3_client.upload_fileobj(file.file, S3_BUCKET_NAME, file_key, ExtraArgs={"ACL": "public-read"})
+    
+    s3_client.upload_fileobj(file.file, S3_BUCKET_NAME, file_key)
 
     s3_url = f"https://{S3_BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{file_key}"
     return s3_url
